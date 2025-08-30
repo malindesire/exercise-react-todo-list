6fcc35dc771c21b1cc9819dd6b267659b612f246
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-08-30 13:13:48 +0200

    Add TodoType

--- todo-list/src/components/Todo/index.tsx
+++ todo-list/src/components/Todo/index.tsx
@@ -1,10 +1,8 @@
+import type { TodoType } from '../../utilities/types';
 import Checkbox from '../Checkbox';
 import styles from './style.module.css';
 
-type TodoProps = {
-	todo: string;
-	done: boolean;
-};
+type TodoProps = TodoType;
 
 export default function Todo({ todo, done }: TodoProps) {
 	return (
