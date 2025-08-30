8d329d10ab8abb2bebfd4fa946318306bacd7dca
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-08-30 13:16:06 +0200

    Add Todos component

---
+++ todo-list/src/components/Todos/index.tsx
@@ -0,0 +1,22 @@
+import type { TodoType } from '../../utilities/types';
+import Todo from '../Todo';
+import styles from './style.module.css';
+
+type TodosProps = {
+	todos: TodoType[];
+};
+
+export default function Todos({ todos }: TodosProps) {
+	return (
+		<ul className={styles.todos}>
+			{todos.map((todo) => (
+				<Todo
+					key={todo.id}
+					id={todo.id}
+					todo={todo.todo}
+					done={todo.done}
+				/>
+			))}
+		</ul>
+	);
+}
