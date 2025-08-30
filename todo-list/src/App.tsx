de19f8a6f9b42bab2c0bc965a6a2f92842d2bef3
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-08-30 13:16:28 +0200

    Add margin to body

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,5 +1,39 @@
+import Todos from './components/Todos';
+
 function App() {
-	return <></>;
+	return (
+		<>
+			<Todos
+				todos={[
+					{
+						id: 'rtl26p',
+						todo: 'Plocka ur diskmaskinen',
+						done: false,
+					},
+					{
+						id: 'gh7k9l',
+						todo: 'Hänga upp tvätten',
+						done: false,
+					},
+					{
+						id: 'sp0m3k',
+						todo: 'Dammsuga källaren',
+						done: true,
+					},
+					{
+						id: 'fks45d',
+						todo: 'Blåsa bort löven i trädgården',
+						done: true,
+					},
+					{
+						id: 'slj2k5',
+						todo: 'Tvätta fönstren',
+						done: true,
+					},
+				]}
+			/>
+		</>
+	);
 }
 
 export default App;
